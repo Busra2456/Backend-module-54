066edb468d84aead1340834a0272167d35228124
Busra2456 <you@example.com>
2025-02-25 14:21:40 +0600

    2nd commit

--- index.js
+++ index.js
@@ -40,5 +40,12 @@ app.listen(port,() =>{
 
 
 /**
+ * 1. create a post api on the server side
+ * 2. client side send data via post
+ * 3. set fetch method inside the fetch options (second parameter) 
+ * 4. option will have three things: method
+ * 5. options will have headers: 'content-type':'application/json'
+ * 6. don't forget to send data dy JSON. Stringify in the body:
+ * 7.on the server side: don't forget to use express.json()
  * 
  */
